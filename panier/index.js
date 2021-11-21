db3bd5afb73b942654a3e410ea93540768febe70
dorianstorella <dorianstorella@gmail.com>
2021-11-21 05:05:25 +0100

    modif avec boucle for et surpression de html

--- panier/index.js
+++ panier/index.js
@@ -1,29 +1,52 @@
-const chaise = document.getElementById("chaise")
-const prixChaise = document.getElementById("prixChaise")
+// const chaise = document.getElementById("chaise")
+// const prixChaise = document.getElementById("prixChaise")
 
-const table = document.getElementById("table")
-const prixTable = document.getElementById("prixTable")
+// const table = document.getElementById("table")
+// const prixTable = document.getElementById("prixTable")
 
-const meuble = document.getElementById("meuble")
-const prixMeuble = document.getElementById("prixMeuble")
+// const meuble = document.getElementById("meuble")
+// const prixMeuble = document.getElementById("prixMeuble")
+
+// let total = document.getElementById("prix_tot").value + 1 - 1
+
+// const addPrixTable = document.getElementById("addTable").addEventListener("click", () => {
+//     total = (total + parseInt(prixTable.innerHTML))
+//     totalprix = document.getElementById("prix_tot")
+    
+//     totalprix.setAttribute("value", total)
+// })
+// const addPrixChaise = document.getElementById("addChaise").addEventListener("click", () => {
+//     total = (total + parseInt(prixChaise.innerHTML))
+//     totalprix = document.getElementById("prix_tot")
+    
+//     totalprix.setAttribute("value", total)
+// })
+// const addPrixMeuble = document.getElementById("addMeuble").addEventListener("click", () => {
+//     total = (total + parseInt(prixMeuble.innerHTML))
+//     totalprix = document.getElementById("prix_tot")
+    
+//     totalprix.setAttribute("value", total)
+// })
+
 
 let total = document.getElementById("prix_tot").value + 1 - 1
+const magasin = document.getElementsByClassName("magasin")
+let lengthMagasin = magasin.length
 
-const addPrixTable = document.getElementById("addTable").addEventListener("click", () => {
-    total = (total + parseInt(prixTable.innerHTML))
-    totalprix = document.getElementById("prix_tot")
-    
-    totalprix.setAttribute("value", total)
-})
-const addPrixChaise = document.getElementById("addChaise").addEventListener("click", () => {
-    total = (total + parseInt(prixChaise.innerHTML))
-    totalprix = document.getElementById("prix_tot")
-    
-    totalprix.setAttribute("value", total)
-})
-const addPrixMeuble = document.getElementById("addMeuble").addEventListener("click", () => {
-    total = (total + parseInt(prixMeuble.innerHTML))
-    totalprix = document.getElementById("prix_tot")
-    
-    totalprix.setAttribute("value", total)
-})
+for( i=0; i < lengthMagasin ;i++) {
+            
+    const prix = [magasin[i].childNodes[3]]
+        
+    const AjoutCart = document.getElementsByTagName("button")[i].addEventListener("click",() => {
+       let content =  prix[0]
+       content = total+=parseInt(content.textContent)
+
+       totalprix = document.getElementById("prix_tot")
+       totalprix.setAttribute("value", content)
+    })  
+}
+
+
+
+
+
