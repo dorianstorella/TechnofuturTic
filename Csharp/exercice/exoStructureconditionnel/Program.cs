3fbc19ab7c94d3eededd1f6c93dff9f51cdabb7c
dorianstorella <dorianstorella@gmail.com>
2021-12-07 13:32:16 +0100

    ajout ex page 114 structure conditionnel

---
+++ Csharp/exercice/exoStructureconditionnel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exoStructureconditionnel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            #region  Demander à l’utilisateur d’encoder 1 nombre (int), si la somme des deux moitiés de celui-ci donne le nombre, afficher « le nombre est paire » sinon « le nombre est impaire 
+            
+            Console.WriteLine("donne un nombre");
+            int nbr1 = int.Parse (Console.ReadLine());
+            
+            if (nbr1 / 2 + nbr1/2 == nbr1 ) {
+                System.Console.WriteLine("nbr pair");
+            } else {
+                System.Console.WriteLine("nbr impair");
+            }
+
+            #endregion
+        }
+    }
+}
